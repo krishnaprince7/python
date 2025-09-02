30d2a1002331beea00f2f5b90c5a547e24742237
krishnaprince7 <princekrishna5707@gmail.com>
2025-09-02 10:01:34 +0530

    first-commit

--- string.py
+++ string.py
@@ -1,3 +1,59 @@
-str = "krishna"
-str2 = "vishwakarma"
-print(str + str2)
+marks = int(input("Enter ur marks:"))
+if(marks >= 90):
+    print("Grade A")
+elif(marks > 80):
+    print("Grade B")
+elif(marks > 70):
+    print("Grade A")
+
+else:
+    print("kya baat he bhi Toper")
+
+
+
+    
+    
+    
+
+
+
+    
+    
+    
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+
+
+
+
+
+
+
+
+
+    
